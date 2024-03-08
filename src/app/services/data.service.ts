4ce591bed08b7b9f3145e7f500a9b80c38b60a0e
BenGau <benoit.gaudieri@drfeel.com>
2024-03-08 12:00:46 +0100

    + data service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../interfaces/post.interface';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  private users: User[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  setUsers(users: User[]): void {
+    this.users = users;
+  }
+
+  getUserById(id: number): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+}
