dd3745b367cb199513b7aaad6a79c6c2d4e2b758
BenGau <benoit.gaudieri@drfeel.com>
2024-03-08 11:59:40 +0100

    + basic spinner and style ui

---
+++ src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [],
+  template:
+    '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>',
+  styleUrl: './spinner.component.scss',
+})
+export class SpinnerComponent {}
