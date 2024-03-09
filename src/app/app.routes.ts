2baf5bc7e5e3cf2eaa9212ce0dd61aca701bf232
BenGau <benoit.gaudieri@drfeel.com>
2024-03-09 12:02:18 +0100

    post routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { PostMainComponent } from './post-main/post-main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'posts',
+    component: PostMainComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/posts',
+  },
+];
