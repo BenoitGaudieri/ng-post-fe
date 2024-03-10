6e18117ba24154f8e51fb6479e7dd73a3b8938a1
BenGau <benoit.gaudieri@drfeel.com>
2024-03-10 16:31:28 +0100

    link to user in details

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Post } from '../../interfaces/post.interface';
 import { User } from '../../interfaces/user.interface';
 import { CardService } from '../../services/card.service';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkWithHref],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.scss',
 })
