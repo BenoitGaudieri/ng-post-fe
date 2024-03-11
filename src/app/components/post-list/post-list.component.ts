60f506f5c0d67642e1e65cac32b17098080c6a4a
BenGau <benoit.gaudieri@drfeel.com>
2024-03-11 15:19:34 +0100

    pagination

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -13,4 +13,32 @@ import { CommonModule } from '@angular/common';
 export class PostListComponent {
   @Input() posts: Post[] = [];
   @Input() isGridView: boolean = false;
+
+  currentPage = 1;
+  postsPerPage = 10;
+
+  // Calculate the number of pages
+  get numberOfPages(): number {
+    return Math.ceil(this.posts.length / this.postsPerPage);
+  }
+
+  // Slice the posts for the current page
+  get paginatedPosts(): Post[] {
+    const startIndex = (this.currentPage - 1) * this.postsPerPage;
+    return this.posts.slice(startIndex, startIndex + this.postsPerPage);
+  }
+
+  // Navigate to the next page
+  nextPage(): void {
+    if (this.currentPage < this.numberOfPages) {
+      this.currentPage++;
+    }
+  }
+
+  // Navigate to the previous page
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
