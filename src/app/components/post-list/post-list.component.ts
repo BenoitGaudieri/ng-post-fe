607edba6296b6ddde211abbb1aa6b2b4024feb4c
BenGau <benoit.gaudieri@drfeel.com>
2024-03-11 15:51:07 +0100

    fix search bug

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Post } from '../../interfaces/post.interface';
 import { PostItemComponent } from '../post-item/post-item.component';
 import { CommonModule } from '@angular/common';
+import { SearchService } from '../../services/search.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
@@ -13,32 +15,42 @@ import { CommonModule } from '@angular/common';
 export class PostListComponent {
   @Input() posts: Post[] = [];
   @Input() isGridView: boolean = false;
-
-  currentPage = 1;
+  currentPage: number = 1;
   postsPerPage = 10;
+  private currentPageSub: Subscription;
 
-  // Calculate the number of pages
+  constructor(private searchService: SearchService) {
+    this.currentPageSub = this.searchService.currentPage$.subscribe((page) => {
+      this.currentPage = page;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.currentPageSub) {
+      this.currentPageSub.unsubscribe();
+    }
+  }
+
+  nextPage(): void {
+    const newPage = this.currentPage + 1;
+    if (newPage <= this.numberOfPages) {
+      this.searchService.setCurrentPage(newPage);
+    }
+  }
+
+  previousPage(): void {
+    const newPage = this.currentPage - 1;
+    if (newPage >= 1) {
+      this.searchService.setCurrentPage(newPage);
+    }
+  }
+
   get numberOfPages(): number {
     return Math.ceil(this.posts.length / this.postsPerPage);
   }
 
-  // Slice the posts for the current page
   get paginatedPosts(): Post[] {
     const startIndex = (this.currentPage - 1) * this.postsPerPage;
     return this.posts.slice(startIndex, startIndex + this.postsPerPage);
-  }
-
-  // Navigate to the next page
-  nextPage(): void {
-    if (this.currentPage < this.numberOfPages) {
-      this.currentPage++;
-    }
-  }
-
-  // Navigate to the previous page
-  previousPage(): void {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-    }
   }
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -7,8 +7,26 @@ import { BehaviorSubject } from 'rxjs';
 export class SearchService {
   private _searchTerm = new BehaviorSubject<string>('');
   searchTerm$ = this._searchTerm.asObservable();
+  private _currentPage = new BehaviorSubject<number>(1);
+  currentPage$ = this._currentPage.asObservable(); // Observable for current page
 
   updateSearchTerm(term: string) {
     this._searchTerm.next(term);
+    this.resetPagination(); // Reset pagination every time a new search term is set
+  }
+
+  // Reset the current page to 1
+  private resetPagination(): void {
+    this._currentPage.next(1);
+  }
+
+  // Update the current page
+  setCurrentPage(page: number): void {
+    this._currentPage.next(page);
+  }
+
+  // Getter for the current page value
+  getCurrentPage(): number {
+    return this._currentPage.value;
   }
 }
