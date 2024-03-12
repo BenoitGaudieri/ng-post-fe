3c55e714dbc0654b32df10917ad54a031c72eb2b
BenGau <benoit.gaudieri@drfeel.com>
2024-03-12 09:55:45 +0100

    cleanup

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -8,24 +8,21 @@ export class SearchService {
   private _searchTerm = new BehaviorSubject<string>('');
   searchTerm$ = this._searchTerm.asObservable();
   private _currentPage = new BehaviorSubject<number>(1);
-  currentPage$ = this._currentPage.asObservable(); // Observable for current page
+  currentPage$ = this._currentPage.asObservable();
 
   updateSearchTerm(term: string) {
     this._searchTerm.next(term);
-    this.resetPagination(); // Reset pagination every time a new search term is set
+    this.resetPagination();
   }
 
-  // Reset the current page to 1
   private resetPagination(): void {
     this._currentPage.next(1);
   }
 
-  // Update the current page
   setCurrentPage(page: number): void {
     this._currentPage.next(page);
   }
 
-  // Getter for the current page value
   getCurrentPage(): number {
     return this._currentPage.value;
   }
